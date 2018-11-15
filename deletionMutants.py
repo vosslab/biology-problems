be5c3fb952c25b8295975e63bf34741b0acb3cd5
Neil Voss <vossman77@yahoo.com>
2018-11-15 08:59:24 -0600

    logic fix: every pair needs to be in a deletion, otherwise you get islands, print the answer, gene count must be between 4 and 20

--- deletionMutants.py
+++ deletionMutants.py
@@ -25,22 +25,27 @@ def list2text(mylist):
 	return mystring
 
 num2word = {
-	1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 
-	6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 
-	11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 
-	15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
+	1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+	6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+	11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+	15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen',
+	20: 'twenty',
 }
-
 
 #====================
 if __name__ == '__main__':
 	if len(sys.argv) >= 2:
 		num_items = int(sys.argv[1])
 	else:
-		num_items = 4
+		num_items = 6
+	if num_items < 4:
+		print("Sorry, you must have at least 4 genes for this program")
+		sys.exit(1)
+	if num_items > 20:
+		print("Sorry, you must have less than 20 genes for this program")
+		sys.exit(1)
 
 	charlist = list("ABCDEFGHJKMPQRSTWXYZ")
-
 	itemlist = charlist[:num_items]
 	random.shuffle(itemlist)
 
@@ -65,44 +70,47 @@ if __name__ == '__main__':
 	iters = 0
 	del_set = []
 	split_gene_pairs = {}
-	used_end_genes = {}
-	while len(split_gene_pairs) < num_sub_sets or len(used_end_genes) < 2:
+	used_gene_pairs = {}
+	while len(split_gene_pairs) < num_sub_sets or len(used_gene_pairs) < num_sub_sets:
 		iters += 1
 		#print("%d of %d gene pairs"%(len(split_gene_pairs), num_sub_sets))
-		delsize = random.randint(min_deletion_size, max_deletion_size)
+		if random.random() < 0.1:
+			delsize = random.randint(min_deletion_size, max_deletion_size)
+		elif len(itemlist) > 4 and random.random() < 0.7:
+			delsize = random.randint(min_deletion_size+2, max_deletion_size)
+		else:
+			delsize = random.randint(min_deletion_size+1, max_deletion_size)
 		delstart = random.randint(0, len(itemlist)-delsize)
 		deletion = itemlist[delstart:delstart+delsize]
-		new_split = 0
+		add_value = 0
 		if delstart > 0:
 			key = itemlist[delstart-1]+itemlist[delstart]
 			#print key
 			if split_gene_pairs.get(key) is None:
 				split_gene_pairs[key] =True
-				new_split += 1
+				add_value += 1
 		if delstart+delsize < len(itemlist):
 			key = itemlist[delstart+delsize-1]+itemlist[delstart+delsize]
 			#print key
 			if split_gene_pairs.get(key) is None:
 				split_gene_pairs[key] =True
-				new_split += 1
-		## need to make sure we include both of the end genes in at least one deletion
-		if deletion[0] == itemlist[0]:
-			gene = deletion[0]
-			if used_end_genes.get(gene) is None:
-				used_end_genes[gene] = True
-				new_split += 1
-		if deletion[-1] == itemlist[-1]:
-			gene = deletion[-1]
-			if used_end_genes.get(gene) is None:
-				used_end_genes[gene] = True
-				new_split += 1
+				add_value += 1
+		## need to make sure every pair of genes is in a least one deletion
+		for j in range(len(deletion)-1):
+			key = deletion[j]+deletion[j+1]
+			if used_gene_pairs.get(key) is None:
+				used_gene_pairs[key] =True
+				add_value += 1
 		deletion.sort()
-		if new_split > 0 and not deletion in del_set:
+		if add_value > 0 and not deletion in del_set:
 			i+=1
 			del_set.append(deletion)
 			print("Del %d: %s"%(i, str(deletion)))
 			#print(split_gene_pairs)
 			#print i, len(used_genes), delsize, delstart, deletion
+	print("")
+	print("Total used gene pairs: %d"%(len(used_gene_pairs)))
+	print("Total split gene pairs: %d"%(len(split_gene_pairs)))
 
 	print("\n##########\n")
 
@@ -114,18 +122,22 @@ if __name__ == '__main__':
 
 	random.shuffle(del_set)
 
-	sys.stdout.write(("A total of %s genes "%(num2word[len(origlist)]))
+	sys.stderr.write("\n")
+	sys.stderr.write(("A total of %s genes "%(num2word[len(origlist)]))
 		+list2text(sortedlist)+" are closely linked in a single chromosome, but "
 		+"their order is unknown. ")
-	sys.stdout.write(("A total of %s deletions in the region are found "%(num2word[len(del_set)]))
+	sys.stderr.write(("A total of %s deletions in the region are found "%(num2word[len(del_set)]))
 		+"to uncover recessive alleles of the genes as follows: ")
 	for i,deletion in enumerate(del_set):
-		sys.stdout.write("Deletion #%d uncovers the %s genes %s; "%(i+1, num2word[len(deletion)], list2text(deletion)))
-	sys.stdout.write("What the correct order for the %s genes? "%(num2word[len(origlist)]))
-	sys.stdout.write("Hint: the first gene on the end is gene %s."%(origlist[0]))
+		sys.stderr.write("Deletion #%d uncovers the %s genes %s; "%(i+1, num2word[len(deletion)], list2text(deletion)))
+	sys.stderr.write("What the correct order for the %s genes? "%(num2word[len(origlist)]))
+	sys.stderr.write("Hint: the first gene on the end is gene %s.\n"%(origlist[0]))
+
+	sys.stderr.write("\n")
+	sys.stderr.write("Answer: %s\n"%(list2string(origlist)))
+	sys.stderr.write("\n")
 
 	#sys.stdout.write("Which one of the following is the correct order for the genes?")
-	
-	
-	print("\n\n##########\n")
+
+	sys.stderr.write("##########\n")
 
