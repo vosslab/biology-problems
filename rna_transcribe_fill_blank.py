5a21627656ce8a4fb92a792df718024f5047dffd
Neil Voss MacBookPro <vossman77@yahoo.com>
2020-09-19 04:46:04 -0500

    new

---
+++ rna_transcribe_fill_blank.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import seqlib
+
+if __name__ == '__main__':
+	if len(sys.argv) >= 2:
+		seqlen = int(sys.argv[1])
+	else:
+		seqlen = 10
+
+	#============================
+	seq = seqlib.makeSequence(seqlen)
+	answer = seqlib.transcribe(seqlib.complement(seq))
+	
+	#============================
+	print("blank 1. What is the RNA sequence to the template strand DNA sequence <span style='font-family: monospace;'>{0}</span>?".format(seq))
+
+	#============================
+	print("A. {0}".format(answer))
+	print("B. {0}".format(seqlib.flip(answer)))
