4ca128dd7e87f7f322c8506eec47abfce1ee4cad
Neil Voss MacBookPro <vossman77@yahoo.com>
2020-10-03 15:56:32 -0500

    updates and fixes

--- who_killer_html.py
+++ who_killer_html.py
@@ -12,18 +12,23 @@ def isKiller(blood, victim, suspect):
 
 if __name__ == '__main__':
 	#structure
-	gel = gellib.GelClass()
+	gel = gellib.GelClassHtml()
 	gel.setTextColumn("Suspect 5")
 
 	num_suspects = 5 #min 3
 	total_bands = 18
 	min_bands = 12
 	max_band_percent = 0.7
-	"""
+
 	num_suspects = 9 #min 3
 	total_bands = 24
 	min_bands = 14
 	max_band_percent = 0.6
+	"""
+	num_suspects = 5 #min 3
+	total_bands = 12
+	min_bands = 2
+	max_band_percent = 0.7
 	"""
 
 	band_tree = gel.createBandTree(total_bands)
@@ -68,25 +73,50 @@ if __name__ == '__main__':
 	for suspect in suspects:
 		suspect.add(random.choice(list(victim)))
 
-	gel.drawLane(allbands, "allbands")
-	gel.blankLane()
-	gel.drawLane(victim, "Victim")
-	gel.drawLane(blood, "Blood")
+	table = ""
+	table += gel.tableWidths()
+	#table += gel.drawLane(allbands, "allbands")
+	table += gel.blankLane()
+	table += gel.drawLane(victim, "Victim")
+	table += gel.drawLane(blood, "Blood")
 	random.shuffle(suspects)
 	random.shuffle(suspects)
+
+	question = "<h6>Based on the DNA gel profile above, which suspect left blood at the crime scene?</h6>"
+
 	killcount = 0
+	choice = ""
 	for i in range(len(suspects)):
 		suspect = suspects[i]
+		table += gel.drawLane(suspect, "Suspect &num;{0:d}".format(i+1))
+
 		iskiller = isKiller(blood, victim, suspect)
-		if iskiller:
-			killcount += 1
 		print(("Suspect #%d: %s"%(i+1, iskiller)))
-		gel.drawLane(suspect, "Suspect %d"%(i+1))
+		choice += "Suspect &num;{0}\t".format(i+1)
+
+		if iskiller:
+			choice += "Correct\t"
+			killcount += 1
+		else:
+			choice += "Incorrect\t"
 	if killcount != 1:
 		"wrong number of killers"
 		sys.exit(1)
-	gel.blankLane()
-	gel.drawLane(canthave, "canthave")
-	gel.drawLane(musthave, "musthave")
-	gel.saveImage("suspects.png")
-	print("open suspects.png")
+	table += gel.blankLane()
+	#table += gel.drawLane(canthave, "canthave")
+	#table += gel.drawLane(musthave, "musthave")
+	table += "</table> "
+	full_question = "<p>Who is the killer?</p> {0} {1}".format(table, question)
+	print("complete\n\n")
+
+	f = open("killer_questions.txt", "a")
+	f.write("MC\t")
+	f.write(full_question+"\t")
+	f.write(choice)
+	f.write("\n")
+	f.close()
+
+	print(question)
+	print(choice)
+
+	print("")
