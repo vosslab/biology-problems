44dd4228a3a85162fd3f551a46ed21aa034419df
Neil Voss MacBookPro <vossman77@yahoo.com>
2020-10-16 09:51:19 -0500

    yet another question generator

---
+++ gametes_unique.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import random
+import genotype
+
+
+if __name__ == '__main__':
+	num_genes = 7
+	num_questions = 99
+	N = 0
+
+	for i in range(num_questions):
+		N += 1
+		number = "{0}. ".format(N)
+		question = ""
+		question += "How many unique gametes could be produced through independent assortment by "
+		gamete_count = 1
+		while gamete_count < 4 or gamete_count > 32:
+			geno, gamete_count = genotype.createGenotype(num_genes)
+		question += "an individual with the genotype {0}?".format(geno)
+
+		f = open('bbq-gametes_unique.txt', 'w')
+		f.write("MC\t{0}\t".format(question))
+		print("{0}. {1}".format(N, question))
+
+		letters = "ABCDEF"
+		for power in range(2, 7):
+			value = 2**power
+			choice = "2<sup>{0:d}</sup> = {1:d}".format(power, value)
+			f.write(choice+'\t')
+			if value == gamete_count:
+				prefix = 'x'
+				f.write('Correct\t')
+			else:
+				prefix = ' '
+				f.write('Incorrect\t')
+			print('- [{0}] {1}. {2}'.format(prefix, letters[power-2], choice))
+		print("")
+		f.write('\n')
+	f.close()
+
--- genetics-problems/genotype.py
+++ genetics-problems/genotype.py
@@ -4,21 +4,33 @@ import sys
 import string
 import random
 
+uppercase = "ABCDEFGHJKMPQRSTVWXYZ"
+lowercase = "abcdefghjkmpqrstvwxyz"
+
+
+def createGenotype(num_genes):
+	genestr = ""
+	gamete_count = 1
+	for i in range(num_genes):
+		val = random.random()
+		if val < 0.25:
+			genestr += uppercase[i] + uppercase[i]
+		elif val < 0.75:
+			genestr += uppercase[i] + lowercase[i]
+			gamete_count *= 2
+		else:
+			genestr += lowercase[i] + lowercase[i]
+		genestr += " "
+	return genestr, gamete_count
+
 if __name__ == '__main__':
 	if len(sys.argv) >= 2:
 		num_genes = int(sys.argv[1])
 	else:
 		num_genes = 3
 
-	genestr = ""
-	for i in range(num_genes):
-		val = random.random()
-		if val < 0.25:
-			genestr += string.uppercase[i] + string.uppercase[i]
-		elif val < 0.75:
-			genestr += string.uppercase[i] + string.lowercase[i]
-		else:
-			genestr += string.lowercase[i] + string.lowercase[i]
-		genestr += "\t"
+	genestr, gamete_count = createGenotype(num_genes)
+	print(gamete_count)
 	print(genestr)
-	print(genestr.replace('\t', ' '))
+	print(genestr.replace(' ', '\t'))
+
