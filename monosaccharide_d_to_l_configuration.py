688970e3eecceffbb52c0604ed75bc00cba7b45c
Neil Voss MacBookPro <vossman77@yahoo.com>
2020-10-26 23:36:14 -0500

    monosaccharide_d_to_l_configuration.py

---
+++ monosaccharide_d_to_l_configuration.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import sys
+import random
+import sugarlib
+
+
+def makeQuestion(sugar_name):
+
+	sugar_code = sugar_codes_class.sugar_name_to_code[sugar_name]
+	sugar_struct = sugarlib.SugarStructure(sugar_code)
+	print(sugar_struct.structural_part_txt())
+	fischer = sugar_struct.Fischer_projection_html()
+
+	question = ''
+	question += 'Above is a Fischer projection of the monosaccharide D-{0}. '.format(sugar_name)
+	question += 'Which one of the following Fischer projections is of the monosaccharide L-{0}? '.format(sugar_name)
+	answer_code = sugar_codes_class.get_enantiomer_code_from_code(sugar_code)
+	choice_codes = [answer_code, ]
+	if sugar_code[0] == 'A':
+		first_stereocarbon = 2
+	else:
+		first_stereocarbon = 3
+	flip_first_OH_code = sugar_codes_class.flip_position(sugar_code, first_stereocarbon)
+	choice_codes.append(flip_first_OH_code)
+
+	flip_last_OH_code = sugar_codes_class.flip_position(sugar_code, 5)
+	choice_codes.append(flip_last_OH_code)
+
+	enantiomer_flip_first_OH_code = sugar_codes_class.flip_position(answer_code, first_stereocarbon)
+	choice_codes.append(enantiomer_flip_first_OH_code)
+
+	enantiomer_flip_last_OH_code = sugar_codes_class.flip_position(answer_code, 5)
+	choice_codes.append(enantiomer_flip_last_OH_code)
+
+	prelen = len(choice_codes)
+	choice_codes = list(set(choice_codes))
+	postlen = len(choice_codes)
+	if prelen != postlen:
+		sys.exit(1)
+
+
+	content = 'MC\t<p>D-{0}</p> '.format(sugar_name)
+	content += fischer
+	content += question
+
+	random.shuffle(choice_codes)
+	for s in choice_codes:
+		my_sugar_struct = sugarlib.SugarStructure(s)
+		my_fischer = my_sugar_struct.Fischer_projection_html()
+		content += "\t"+my_fischer
+		if s == answer_code:
+			content += "\tCorrect"
+		else:
+			content += "\tIncorrect"
+	return content
+
+if __name__ == '__main__':
+	sugar_codes_class = sugarlib.SugarCodes()
+	D_hexose_names = sugar_codes_class.get_D_hexoses()
+	#D_hexose_names = sugar_codes_class.get_D_aldohexoses()
+	f = open('bbq-monosaccharide_d_to_l_configuration.txt', 'w')
+	for sugar_name in D_hexose_names:
+		#random.shuffle(D_hexose_names)
+		#sugar_name = D_hexose_names.pop()
+		print(sugar_name)
+		content = makeQuestion(sugar_name)
+		f.write(content+'\n')
+	f.close()
+
--- sugarlib.py
+++ sugarlib.py
@@ -1,78 +1,183 @@
 ### Library for crateing HTML tables of Sugar molecules
 
+import copy
+
 ### nomenclature for sugar code
 # A or M - aldose (A) or ketose (M) with a hydroxymethyl group
 # L, R, or K - left, right, or carboxyl
 # L, D - L sugar or D sugar
 # M - nonstereo symmetric carbon, M = Hydroxymethyl group
 
-sugar_table = {
-	'ADM': 'glyceraldehyde',
-	'MKM': 'dihydroxacetone',
-	
-	'ARDM': 'erythrose',
-	'ALDM': 'threose',
-	'MKDM': 'erythrulose',
-	
-	'ARRDM': 'ribose',
-	'ALRDM': 'arabinaose',
-	'ARLDM': 'xylose',
-	'ALLDM': 'lyxose',
-	'MKRDM': 'ribulose',
-	'MKLDM': 'xylulose',
-	'MRKDM': 'meso 3-ketopentose',
-	'MLKDM': 'D-3-ketopentose',
-	'MRKLM': 'L-3-ketopentose',
+#unless specified the following are all D sugars
+
+class SugarCodes(object):
+	def __init__(self):
+		self.sugar_code_to_name = {
+			'ADM': 'glyceraldehyde',
+			'MKM': 'dihydroxacetone',
 		
-	'ARRRDM': 'allose',
-	'ALRRDM': 'altrose',
-	'ARLRDM': 'glucose',
-	'ALLRDM': 'mannose',
-	'ARRLDM': 'gulose',
-	'ALRLDM': 'idose',
-	'ARLLDM': 'galactose',
-	'ALLLDM': 'talose',
-	'MKLLDM': 'psicose',
-	'MKLRDM': 'fructose',
-	'MKRLDM': 'sorbose',
-	'MKRRDM': 'tagatose',
-}
+			'ARDM': 'erythrose',
+			'ALDM': 'threose',
+			'MKDM': 'erythrulose',
+
+			'ARRDM': 'ribose',
+			'ALRDM': 'arabinaose',
+			'ARLDM': 'xylose',
+			'ALLDM': 'lyxose',
+			'MKRDM': 'ribulose',
+			'MKLDM': 'xylulose',
+			'MRKDM': 'meso 3-ketopentose',
+			'MLKDM': 'D-3-ketopentose',
+			'MRKLM': 'L-3-ketopentose',
+
+			'ARRRDM': 'allose',
+			'ALRRDM': 'altrose',
+			'ARLRDM': 'glucose',
+			'ALLRDM': 'mannose',
+			'ARRLDM': 'gulose',
+			'ALRLDM': 'idose',
+			'ARLLDM': 'galactose',
+			'ALLLDM': 'talose',
+			'MKLLDM': 'psicose',
+			'MKLRDM': 'fructose',
+			'MKRLDM': 'sorbose',
+			'MKRRDM': 'tagatose',
+		}
+		self.get_names_from_codes()
+
+	#============================
+	def get_names_from_codes(self):
+		self.sugar_name_to_code = {}
+		for code in self.sugar_code_to_name:
+			name = self.sugar_code_to_name[code]
+			self.sugar_name_to_code[name] = code
+
+	#============================
+	def get_D_hexoses(self):
+		D_hexose_names = []
+		for code in self.sugar_code_to_name:
+			if len(code) == 6:
+				name = self.sugar_code_to_name[code]
+				D_hexose_names.append(name)
+		return D_hexose_names
+
+	#============================
+	def get_D_aldohexoses(self):
+		D_aldohexose_names = []
+		for code in self.sugar_code_to_name:
+			if len(code) == 6 and code[0] == 'A':
+				name = self.sugar_code_to_name[code]
+				D_aldohexose_names.append(name)
+		return D_aldohexose_names
+
+	#============================
+	def flip_position(self, sugar_code, position):
+		#position starts at 1 for the carbon number, not the string index
+		flipped_code = list(sugar_code)
+		length = len(sugar_code)
+		if position == 1 or position == length:
+			print("Um, the first and last carbon aren't stereosymmetric")
+			return None
+		elif position == length-1:
+			#configuration carbon
+			if sugar_code[position-1] == 'D':
+				flipped_code[position-1] = 'L'
+			elif sugar_code[position-1] == 'L':
+				flipped_code[position-1] = 'D'
+		else:
+			if sugar_code[position-1] == 'R':
+				flipped_code[position-1] = 'L'
+			elif sugar_code[position-1] == 'L':
+				flipped_code[position-1] = 'R'
+		#print("Flipped {0} to {1} at position {2:d}".format(sugar_code[position-1], flipped_code[position-1], position))
+		return ''.join(flipped_code)
 
 
+	#============================
+	def get_enantiomer_code_from_name(self, sugar_name):
+		sugar_code = self.sugar_name_to_code[sugar_name]
+		return get_enantiomer_code_from_code(sugar_code)
 
+	#============================
+	def get_enantiomer_code_from_code(self, sugar_code):
+		code_list = list(sugar_code)
+		# first is the same
+		enantiomer_code = [code_list.pop(0),]
+		# last is the same, but save for later
+		last_carbon = code_list.pop(-1)
+		# next-to-last carbon is special
+		penultimate_carbon = code_list.pop(-1)
+		for code in code_list:
+			if code == 'L':
+				enantiomer_code.append('R')
+			elif code == 'R':
+				enantiomer_code.append('L')
+			elif code == 'K':
+				enantiomer_code.append('K')
+		if penultimate_carbon == 'L':
+			enantiomer_code.append('D')
+		elif penultimate_carbon == 'D':
+			enantiomer_code.append('L')
+		elif penultimate_carbon == 'K':
+			enantiomer_code.append('K')
+		enantiomer_code.append(last_carbon)
+		return ''.join(enantiomer_code)
+
+#============================
+#============================
+#============================
+
+#============================
 def dextrose():
-	return D_glucose()
+	return glucose()
 
-def D_glucose():
-	sugar_code = "ALRLDM"
-	sugar = SugarClass(sugar_code)
-	print(sugar.molecular_formula_txt())
-	sugar.generate_structral_formula()
-	print(sugar.structural_parts)
-	return sugar
+#============================
+def glucose():
+	sugarcodes = SugarCodes()
+	sugar_code = sugarcodes.sugar_name_to_code['glucose']
+	sugar_struct = SugarStructure(sugar_code)
+	sugar_struct.sugar_summary()
+	return sugar_struct
 
-def D_galactose():
-	sugar_code = "ARLLDM"
-	sugar = SugarClass(sugar_code)
-	print(sugar.molecular_formula_txt())
-	sugar.generate_structral_formula()
-	print(sugar.structural_parts)
-	return sugar
+#============================
+def galactose():
+	sugarcodes = SugarCodes()
+	sugar_code = sugarcodes.sugar_name_to_code['galactose']
+	sugar_struct = SugarStructure(sugar_code)
+	sugar_struct.sugar_summary()
+	return sugar_struct
 
-def D_fructose():
-	sugar_code = "KKLRDM"
-	sugar = SugarClass(sugar_code)
-	print(sugar.molecular_formula_txt())
-	sugar.generate_structral_formula()
-	print(sugar.structural_parts)
-	return sugar
+#============================
+def fructose():
+	sugarcodes = SugarCodes()
+	sugar_code = sugarcodes.sugar_name_to_code['fructose']
+	sugar_struct = SugarStructure(sugar_code)
+	sugar_struct.sugar_summary()
+	return sugar_struct
 
-class SugarClass(object):
+#============================
+#============================
+#============================
+class SugarStructure(object):
+	#============================
 	def __init__(self, sugar_code):
 		self.sugar_code = sugar_code
 		self.molecular_formula_ready = False
-		self.structural_formula_ready = False
+		self.structural_parts = []
 
+	#============================
+	def sugar_summary(self):
+		print(self.molecular_formula_txt())
+		print(self.structural_part_txt())
+		print(self.Fischer_projection_html())
+
+	#============================
+	def structural_part_txt(self):
+		if len(self.structural_parts) == 0:
+			self.generate_structral_formula()
+		return "--".join(self.structural_parts)
+
+	#============================
 	def generate_structral_formula(self):
 		self.structural_parts = []
 		self.structural_parts.append("H")
@@ -96,7 +201,7 @@ class SugarClass(object):
 			self.structural_parts.append("CHOH")
 		self.structural_parts.append("H")
 
-
+	#============================
 	def generate_molecular_formula(self):
 		self.carbons = 0
 		self.oxygens = 0
@@ -127,8 +232,9 @@ class SugarClass(object):
 		if last_carbon == "M":
 			self.carbons += 1
 			self.oxygens += 1
-			self.hydrogens += 3	
+			self.hydrogens += 3
 
+	#============================
 	def molecular_formula_html(self):
 		if self.molecular_formula_ready is False:
 			self.generate_molecular_formula()
@@ -137,8 +243,8 @@ class SugarClass(object):
 		molecular_formula += "H<sub>{0:d}</sub>".format(self.hydrogens)
 		molecular_formula += "O<sub>{0:d}</sub>".format(self.oxygens)
 		return molecular_formula
-	
 
+	#============================
 	def molecular_formula_txt(self):
 		if self.molecular_formula_ready is False:
 			self.generate_molecular_formula()
@@ -147,6 +253,122 @@ class SugarClass(object):
 		molecular_formula += "H{0:d} ".format(self.hydrogens)
 		molecular_formula += "O{0:d} ".format(self.oxygens)
 		return molecular_formula
-	
 
+	#============================
+	def _aldose_html_header(self):
+		aldose_header = ''
+		aldose_header += '<tr>'
+		aldose_header += ' <td colspan="2" style="text-align: right;">H&nbsp;</td>'
+		aldose_header += ' <td colspan="2"></td>'
+		aldose_header += ' <td colspan="2" style="text-align: left;">&nbsp;O</td>'
+		aldose_header += '</tr><tr>'
+		aldose_header += ' <td></td>'
+		aldose_header += ' <td colspan="2" style="text-align: right;">\&nbsp;</td>'
+		aldose_header += ' <td colspan="2" style="text-align: left;">&nbsp;//</td>'
+		aldose_header += ' <td></td>'
+		aldose_header += '</tr><tr>'
+		aldose_header += ' <td></td>'
+		aldose_header += ' <td></td>'
+		aldose_header += ' <td colspan="2" style="text-align: center;">C</td>'
+		aldose_header += ' <td></td>'
+		aldose_header += ' <td></td>'
+		aldose_header += '</tr>'
+		return aldose_header
+
+	#============================
+	def _hydroxymethyl_html_header(self):
+		hydroxymethyl_header = ''
+		hydroxymethyl_header += self._hydroxymethyl_html_cap()
+		return hydroxymethyl_header
+
+	#============================
+	def _hydrogen_html_cap(self):
+		hydrogen_cap = ''
+		hydrogen_cap += '<tr>'
+		hydrogen_cap += ' <td></td>'
+		hydrogen_cap += ' <td></td>'
+		hydrogen_cap += ' <td colspan="2" style="text-align: center;">H</td>'
+		hydrogen_cap += ' <td></td>'
+		hydrogen_cap += ' <td></td>'
+		hydrogen_cap += '</tr>'
+		return hydrogen_cap
+
+	#============================
+	def _hydroxymethyl_html_cap(self):
+		hydroxymethyl_cap = ''
+		hydroxymethyl_cap += '<tr>'
+		hydroxymethyl_cap += ' <td></td>'
+		hydroxymethyl_cap += ' <td></td>'
+		hydroxymethyl_cap += ' <td colspan="4" style="text-align: left;">CH<sub>2</sub>OH</td>'
+		hydroxymethyl_cap += '</tr>'
+		return hydroxymethyl_cap
+
+	#============================
+	def _html_top_connector(self, extra_columns=False):
+		top_connector = ''
+		top_connector += '<tr>'
+		if extra_columns is True:
+			top_connector += ' <td></td>'
+		top_connector += ' <td style="border-top: solid black 1px;"></td>'
+		top_connector += ' <td style="border-right: solid black 1px; border-top: solid black 1px;"></td>'
+		top_connector += ' <td style="border-left: solid black 1px; border-top: solid black 1px;"></td>'
+		top_connector += ' <td style="border-top: solid black 1px;"></td>'
+		if extra_columns is True:
+			top_connector += ' <td></td>'
+		top_connector += '</tr>'
+		return top_connector
+
+	#============================
+	def _html_bottom_content(self, configuration='D'):
+		if configuration.upper() == 'D' or configuration.upper() == 'R':
+			content = ['H', 'OH']
+		elif configuration.upper() == 'L':
+			content = ['HO', 'H']
+		bottom_content = ''
+		bottom_content += '<tr>'
+		bottom_content += ' <td rowspan="2" style="text-align: right;">{0}</td>'.format(content[0])
+		bottom_content += ' <td style="border-bottom: solid black 1px;"></td>'
+		bottom_content += ' <td style="border-right: solid black 1px; border-bottom: solid black 1px;"></td>'
+		bottom_content += ' <td style="border-left: solid black 1px; border-bottom: solid black 1px;"></td>'
+		bottom_content += ' <td style="border-bottom: solid black 1px;"></td>'
+		bottom_content += ' <td rowspan="2" style="text-align: left;">{0}</td>'.format(content[1])
+		bottom_content += '</tr>'
+		return bottom_content
+
+	#============================
+	def _html_carboxyl(self):
+		bottom_content = ''
+		bottom_content += '<tr>'
+		bottom_content += ' <td style="border-right: solid black 1px;" colspan="3"></td>'
+		bottom_content += ' <td style="border-left: solid black 1px;" colspan="3">'
+		bottom_content += '  <span style="font-size: medium;">==</span>O<span style="font-size: x-large;">&nbsp;</span></td>'
+		bottom_content += '</tr>'
+		return bottom_content
+
+	#============================
+	def Fischer_projection_html(self):
+		table = ''
+		table += '<table border="1" style="border-collapse: collapse; border: white solid 0px;">'
+		table += '<tbody>'
+		code_list = list(self.sugar_code)
+		first_code = code_list.pop(0)
+		if first_code == 'A':
+			table += self._aldose_html_header()
+		elif first_code == 'M':
+			table += self._hydroxymethyl_html_header()
+		#remove last carbon before loop
+		last_carbon = code_list.pop(-1)
+		# loop over middle carbons
+		for code in code_list:
+			if code == "L" or code == "R" or code == "D":
+				table += self._html_bottom_content(code)
+				table += self._html_top_connector()
+			elif code == "K":
+				#not sure what to do
+				table += self._html_carboxyl()
+		#last_carbon
+		if last_carbon == "M":
+			table += self._hydroxymethyl_html_cap()
+		table += '</table>'
+		return table
 
