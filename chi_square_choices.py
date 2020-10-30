389352150ae8ab5c85c7f71d320873680a4e4817
Neil Voss MacBookPro <vossman77@yahoo.com>
2020-10-30 14:26:50 -0500

    starting a new problem, want to save progress, but not finished or tested

--- chi_square_choices.py
+++ chi_square_choices.py
@@ -7,35 +7,29 @@ import random
 from scipy.stats.distributions import chi2
 
 ### types of errors
-# 0 * divide by the observed instead of expected
-# 1 * square the bottom number
-# 2 * square the bottom observed number
-# 3 * forget to square the numbers before division
-# 4 * use the wrong null hypothesis
-# 5 * wrong degrees of freedom 3 vs 4
-# 6 * use chi-sq value being less critical value -> reject
-# 7 * wrong degrees of freedom 3 vs 2
-# * use chi-sq value being less than 0.05 to reject
-# * take a p > 0.05 as reject
+# show 3 tables of chi square calculations
+# - table 1 the real answer
+# - table 2 divide by the observed
+# - table 3 random choice of
+# - - divide by observed squared
+# - - forget to square the top divide by observed squared
+# - - forget to square the top divide by observed
+# student chooses which one is correct, and decides whether or not to reject the null.
 
-error2choice = {
-	0: 0,
-	1: 0,
-	2: 0,
-	3: 1,
-	4: 3,
-	5: 4,
-	6: 2,
-	7: 4,
-	8: 2,
+error_types = {
+	0: 'divide by observed squared',
+	1: 'forget to square the top divide by observed squared',
+	2: 'forget to square the top divide by observed',
 }
 
 choices = [
-	'the wrong numbers in the calculation were used for division',
-	'the numbers in the calculation have to be squared',
-	'the wrong rejection criteria was used',
-	'the expected progeny for the null hypothesis is incorrect',
-	'the degrees of freedom is wrong',
+	'table 1 is correct and we <b>reject</b> the null hypothesis',
+	'table 2 is correct and we <b>reject</b> the null hypothesis',
+	'table 3 is correct and we <b>reject</b> the null hypothesis',
+
+	'table 1 is correct and we <b>accept</b> the null hypothesis',
+	'table 2 is correct and we <b>accept</b> the null hypothesis',
+	'table 3 is correct and we <b>accept</b> the null hypothesis',
 ]
 
 #===================
@@ -47,10 +41,10 @@ def get_p_value(chisq, df):
 
 #===================
 #===================
-def get_chisq_value(pvalue, df):
-	chisq = chi2.ppf(1.0 - float(pvalue), int(df))
+def get_critical_value(alpha_criterion, df):
+	critical_value = chi2.ppf(1.0 - float(alpha_criterion), int(df))
 	#print(chisq)
-	return float(chisq)
+	return float(critical_value)
 
 #===================
 #===================
@@ -84,10 +78,10 @@ def createObservedProgeny(N=160, ratio="9:2:4:1"):
 
 #===================
 #===================
-def divideByObservedError():
+def divideByObservedError(ratio):
 	observed = [90]
 	while 88 <= observed[0] <= 92 or 9 <= observed[3] <= 11:
-		observed = createObservedProgeny()
+		observed = createObservedProgeny(ratio=ratio)
 	expected = [90,30,30,10]
 	stats_list = []
 	chisq = 0.0
@@ -110,10 +104,10 @@ def divideByObservedError():
 
 #===================
 #===================
-def divideByObservedAndSquareError():
+def divideByObservedAndSquareError(ratio):
 	observed = [90]
 	while 88 <= observed[0] <= 92 or 9 <= observed[3] <= 11:
-		observed = createObservedProgeny()
+		observed = createObservedProgeny(ratio=ratio)
 	expected = [90,30,30,10]
 	stats_list = []
 	chisq = 0.0
@@ -136,36 +130,10 @@ def divideByObservedAndSquareError():
 
 #===================
 #===================
-def divideBySquareError():
+def noSquareError(ratio):
 	observed = [90]
 	while 88 <= observed[0] <= 92 or 9 <= observed[3] <= 11:
-		observed = createObservedProgeny()
-	expected = [90,30,30,10]
-	stats_list = []
-	chisq = 0.0
-	for j in range(len(observed)):
-		row = []
-		obs = observed[j]
-		exp = expected[j]
-		row.append(exp)
-		row.append(obs)
-		chirow = (obs-exp)**2/float(exp**2)
-		calc = "<sup>({0}-{1})<sup>2</sup></sup>&frasl;&nbsp;<sub>{2}<sup>2</sup></sub>".format(obs, exp, exp)
-		row.append(calc)
-		chistr = "{0:.3f}".format(chirow)
-		row.append(chistr)
-		chisq += chirow
-		stats_list.append(row)
-	chistr = "{0:.3f}".format(chisq)
-	stats_list.append(chistr)
-	return stats_list
-
-#===================
-#===================
-def noSquareError():
-	observed = [90]
-	while 88 <= observed[0] <= 92 or 9 <= observed[3] <= 11:
-		observed = createObservedProgeny()
+		observed = createObservedProgeny(ratio=ratio)
 	expected = [90,30,30,10]
 	stats_list = []
 	chisq = 0.0
@@ -188,10 +156,10 @@ def noSquareError():
 
 #===================
 #===================
-def normalGoodStats():
+def normalGoodStats(ratio):
 	observed = [90]
 	while 88 <= observed[0] <= 92 or 9 <= observed[3] <= 11:
-		observed = createObservedProgeny()
+		observed = createObservedProgeny(ratio=ratio)
 	expected = [90,30,30,10]
 	stats_list = []
 	chisq = 0.0
@@ -212,31 +180,7 @@ def normalGoodStats():
 	stats_list.append(chistr)
 	return stats_list
 
-#===================
-#===================
-def wrongNullHypothesis():
-	observed = [90]
-	while observed[0] >= 70 or observed[3] <= 20:
-		observed = createObservedProgeny(ratio="7:5:3:2")
-	expected = [40,40,40,40]
-	stats_list = []
-	chisq = 0.0
-	for j in range(len(observed)):
-		row = []
-		obs = observed[j]
-		exp = expected[j]
-		row.append(exp)
-		row.append(obs)
-		chirow = (obs-exp)**2/float(exp)
-		calc = "<sup>({0}-{1})<sup>2</sup></sup>&frasl;&nbsp;<sub>{2}</sub>".format(obs, exp, exp)
-		row.append(calc)
-		chistr = "{0:.3f}".format(chirow)
-		row.append(chistr)
-		chisq += chirow
-		stats_list.append(row)
-	chistr = "{0:.3f}".format(chisq)
-	stats_list.append(chistr)
-	return stats_list
+
 
 #===================
 #===================
@@ -312,7 +256,7 @@ def make_chi_square_table():
 		table += "<tr>"
 		table += " <th align='center' style='background-color: silver'>{0:d}</th>".format(df)
 		for p in p_values:
-			chisq = get_chisq_value(p, df)
+			chisq = get_critical_value(p, df)
 			table += " <td align='center'>{0:.2f}</td>".format(chisq)
 		table += "</tr>"
 
@@ -321,100 +265,92 @@ def make_chi_square_table():
 
 #===================
 #===================
-def chiSquareResults(chisq, critical_value, flip):
-	if flip is False:
-		if chisq > critical_value:
-			results = ("greater", "rejected")
-		elif chisq <= critical_value:
-			results = ("less", "accepted")
-	else:
-		# this is WRONG
-		if chisq > critical_value:
-			results = ("greater", "accepted")
-		elif chisq <= critical_value:
-			results = ("less", "rejected")
-	return results
-
-#===================
-#===================
-def questionContent(chisq, df, alpha, flip=False):
-
-	pvalue = get_p_value(chisq, df)
-	critical_value = get_chisq_value(alpha, df)
-
-	question = ""
-	question += "<p>The final result gives the chi-squared (&chi;<sup>2</sup>) test value of {0:.2f} with {1} degrees of freedom. ".format(chisq, df)
-	question += "Using the Table of &chi;<sup>2</sup> Critical Values and a level of significance &alpha;={0:.2f}, we get a critical value of {1:.2f}. ".format(alpha, critical_value)
-	#question += "They calculated a p-value of {2:.3f} ({3:.1f}%). ".format(chisq, df, pvalue, pvalue*100)
-	#question += "Since the p-value was {0} than the level of significance &alpha;=0.05 (5%), the null hypothesis was {1}.</p>".format("less", "rejected")
-	print("chi_square     = {0:.3f}".format(chisq))
-	print("critical_value = {0:.3f}".format(critical_value))
-
-	results = chiSquareResults(chisq, critical_value, flip)
-
-	question += "Since the chi-squared (&chi;<sup>2</sup>) test value of {0:.2f} is {1} than the critical value of {2:.2f}, the null hypothesis was {3}.</p>".format(chisq, results[0], critical_value, results[1])
-
-	question += "<hr/> "
-
-	question += "<p>Your lab partner has done a chi-squared (&chi;<sup>2</sup>) test for your lab data (above), "
+def questionContent():
+	question = ''
+	question += "<p>Your lab partner is trying again and did another a chi-squared (&chi;<sup>2</sup>) test for your lab data (above).</p>"
 	question += "for the F<sub>2</sub> generation in a standard dihybid cross. They wanted to know if "
-	question += "the results confirm the expected phenotype ratios, "
-	question += "but as usual they did something wrong. <strong>What did they do wrong?</strong></p>"
+	question += "the results confirm the expected phenotype ratios</p>"
+
+	question += "<p>You helped them set up the null hypothesis, so you know it is correct, "
+	question += "but they were so unsure about how to calculate the chi-squared (&chi;<sup>2</sup>) value "
+	question += "that they did it three (3) different ways. "
+	question += "Now they want you to tell them which table is correct and "
+	question += "whether they can accept or reject the null hypothesis."
+	question += "Before you ask your instructor for a new lab partner, please help them out. "
 	return question
 
-
-### types of errors
-# 0 * divide by the observed instead of expected
-# 1 * square the bottom number
-# 2 * square the bottom observed number
-# 3 * forget to square the numbers before division
-# 4 * use the wrong null hypothesis
-# 5 * wrong degrees of freedom 3 vs 4
-# * use chi-sq value being less than 0.05 to reject
-# * take a p > 0.05 as reject
+#===================
+#===================
+def getChiSquareResult(final_chisq, df, alpha):
+	critical_value = get_critical_value(alpha, df)
+	if final_chisq > critical_value:
+		return 'reject_null'
+	elif final_chisq <= critical_value:
+		return 'accept_null'
+	return None
 
 #===================
 #===================
-def makeQuestion(error_type):
+def makeQuestion(error_type, desired_result):
+	"""
+	error type to NOT include
 
-	if error_type == 0:
-		stats_list = divideByObservedError()
-	elif error_type == 1:
-		stats_list = divideBySquareError()
-	elif error_type == 2:
-		stats_list = divideByObservedAndSquareError()
-	elif error_type == 3:
-		stats_list = noSquareError()
-	elif error_type == 4:
-		stats_list = wrongNullHypothesis()
-	else:
-		stats_list = normalGoodStats()
+	0: 'divide by observed squared',
+	1: 'forget to square the top divide by observed squared',
+	2: 'forget to square the top divide by observed',
+	"""
 
-	if error_type == 5:
-		df = 4
-	elif error_type == 7:
-		df = 2
-	else:
-		df = 3
+	if desired_result == 'reject':
+		ratio = '7:2:5:2'
+	elif desired_result == 'accept':
+		ratio = '9:3:3:1'
 
-	if error_type == 6:
-		flip = True
-	else:
-		flip = False
-
-	if error_type == 8:
-		alpha = 0.5
-	else:
-		alpha = 0.05
-
-	chisq = float(stats_list[-1])
-	table1 = createDataTable(stats_list)
 	chi_square_table = make_chi_square_table()
-	question = questionContent(chisq, df, alpha, flip)
-	complete_question = chi_square_table+"<br/>"+table1+"<br/>"+question
 
-	return complete_question
+	stats_list = normalGoodStats(ratio)
+	answer_table = createDataTable(stats_list)
 
+	numbers_tables = []
+	i = -1
+	print(error_type)
+	for method in (divideByObservedError, divideByObservedAndSquareError, noSquareError):
+		i += 1
+		print(i)
+		if i == error_type:
+			continue
+		stats_list = method(ratio)
+		wrong_table = createDataTable(stats_list)
+		numbers_tables.append(wrong_table)
+
+	#tabke the tables and shuffle them
+	numbers_tables.append(answer_table)
+	shuffle_map = list(range(len(numbers_tables)))
+	random.shuffle(shuffle_map)
+	print(shuffle_map)
+	shuffled_tables = []
+	for index in shuffle_map:
+		shuffled_tables.append(numbers_tables[index])
+
+	#use the real values
+	final_chisq = float(stats_list[-1])
+	df = 3
+	alpha = 0.05
+	result = getChiSquareResult(final_chisq, df, alpha)
+	print(result)
+	answer_num = shuffle_map[2]
+	if result == 'accept_null':
+		answer_num += 3
+
+	# write the question content
+	question = questionContent()
+
+	complete_question = chi_square_table+" <br/> "
+	for number_table in shuffled_tables:
+		complete_question += number_table+" <br/> "
+	complete_question += " <hr/> "
+	complete_question += question
+
+	return complete_question, answer_num
 
 
 #===================
@@ -422,26 +358,25 @@ def makeQuestion(error_type):
 #===================
 #===================
 if __name__ == '__main__':
-	duplicates = 11
-	max_error_types = len(error2choice)
+	duplicates = 1
 	letters = "ABCDEFGHI"
-	f = open("bbq-chi_square_errors.txt", "w")
+	f = open("bbq-chi_square_choices.txt", "w")
 	for i in range(duplicates):
-		for error_type in range(max_error_types):
-			print("")
-			complete_question = makeQuestion(error_type)
-			f.write("MC\t{0}".format(complete_question))
-			choice_index = error2choice[error_type]
-			answer = choices[choice_index]
-			choices_copy = copy.copy(choices)
-			random.shuffle(choices_copy)
-			for k, c in enumerate(choices_copy):
-				if c == answer:
-					prefix = "*"
-					status = "Correct"
-				else:
-					prefix = ""
-					status = "Incorrect"
-				f.write("\t{0}\t{1}".format(c, status))
-				print("{0}{1}. {2}".format(prefix, letters[k], c))
-			f.write("\n")
+		for error_type in error_types:
+			for desired_result in ('accept', 'reject'):
+				print("")
+				complete_question, answer_num = makeQuestion(error_type, desired_result)
+				f.write("MC\t{0}".format(complete_question))
+				answer = choices[answer_num]
+				choices_copy = copy.copy(choices)
+				#random.shuffle(choices_copy)
+				for k, c in enumerate(choices_copy):
+					if c == answer:
+						prefix = "*"
+						status = "Correct"
+					else:
+						prefix = ""
+						status = "Incorrect"
+					f.write("\t{0}\t{1}".format(c, status))
+					print("{0}{1}. {2}".format(prefix, letters[k], c))
+				f.write("\n")
