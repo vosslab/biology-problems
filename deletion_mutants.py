0a1441760b5437c8409637be567dbdec9a83a9d4
Neil Voss MacBookPro <vossman77@yahoo.com>
2020-11-29 15:22:10 -0600

    clean up code, add html table

--- deletion_mutants.py
+++ deletion_mutants.py
@@ -7,6 +7,52 @@ import copy
 import random
 import pprint
 
+def makeHtmlTable(origlist, del_set):
+	table = ''
+	table += '<table style="border-collapse: collapse; border: 2px solid black; '
+	width = 60*(len(origlist) + 1)
+	height = 25*(len(del_set) + 1)
+	table += 'width: {0:d}px; height: {1:d}px;">'.format(width, height)
+	table += '<tr><th> </th>'
+	for i in range(len(origlist)):
+		table += '<th align="center">Gene {0}</th>'.format(i+1)
+	table += '</tr>'
+	colors = ['darkred', 'DarkSlateGray', 'DarkGreen', 'Indigo', 'MidnightBlue', 'DarkOliveGreen', ]
+	for i,deletion in enumerate(del_set):
+		table += '<tr><th align="center">Del #{0}</th>'.format(i+1)
+		for gene in origlist:
+			if gene in deletion:
+				#black fill
+				table += '<td bgcolor="{0}"> </td>'.format(colors[i % len(colors)])
+			else:
+				table += '<td bgcolor="#EEEEEE"> </td>'
+		table += '</tr>'
+	table += '</table>'
+	return table
+
+def writeQuestion(origlist, del_set):
+	sortedlist = copy.copy(origlist)
+	sortedlist.sort()
+
+	sys.stderr.write("\n")
+	sys.stderr.write(("A total of %s genes "%(num2word[len(origlist)]))
+		+list2text(sortedlist)+" are closely linked in a single chromosome, but "
+		+"their order is unknown. ")
+	sys.stderr.write(("A total of %s deletions in the region are found "%(num2word[len(del_set)]))
+		+"to uncover recessive alleles of the genes as follows: ")
+	for i,deletion in enumerate(del_set):
+		sys.stderr.write("Deletion #%d uncovers the %s genes %s; "%(i+1, num2word[len(deletion)], list2text(deletion)))
+	sys.stderr.write("What the correct order for the %s genes? "%(num2word[len(origlist)]))
+	sys.stderr.write("Hint: the first gene on the end is gene %s.\n"%(origlist[0]))
+
+	sys.stderr.write("\n")
+	sys.stderr.write("Answer: %s\n"%(list2string(origlist)))
+	sys.stderr.write("\n")
+
+	#sys.stdout.write("Which one of the following is the correct order for the genes?")
+
+	sys.stderr.write("##########\n")
+
 def list2string(mylist):
 	mystring = ""
 	for letter in mylist:
@@ -32,19 +78,8 @@ num2word = {
 	20: 'twenty',
 }
 
-#====================
-if __name__ == '__main__':
-	if len(sys.argv) >= 2:
-		num_items = int(sys.argv[1])
-	else:
-		num_items = 6
-	if num_items < 4:
-		print("Sorry, you must have at least 4 genes for this program")
-		sys.exit(1)
-	if num_items > 20:
-		print("Sorry, you must have less than 20 genes for this program")
-		sys.exit(1)
 
+def makeDeletions(num_items):
 	charlist = list("ABCDEFGHJKMPQRSTWXYZ")
 	itemlist = charlist[:num_items]
 	random.shuffle(itemlist)
@@ -53,9 +88,7 @@ if __name__ == '__main__':
 	if itemlist[0] > itemlist[-1]:
 		itemlist.reverse()
 	origlist = copy.copy(itemlist)
-	sortedlist = copy.copy(itemlist)
-	sortedlist.sort()
-	print(itemlist)
+	print("itemlist", itemlist)
 
 	num_deletions = 5
 	min_deletion_size = 2
@@ -113,6 +146,22 @@ if __name__ == '__main__':
 	print(("Total split gene pairs: %d"%(len(split_gene_pairs))))
 
 	print("\n##########\n")
+	return origlist, del_set
+
+#====================
+if __name__ == '__main__':
+	if len(sys.argv) >= 2:
+		num_items = int(sys.argv[1])
+	else:
+		num_items = 6
+	if num_items < 4:
+		print("Sorry, you must have at least 4 genes for this program")
+		sys.exit(1)
+	if num_items > 20:
+		print("Sorry, you must have less than 20 genes for this program")
+		sys.exit(1)
+
+	origlist, del_set = makeDeletions(num_items)
 
 	### write question
 	#Genes a, b, c, d, e, and f are closely linked in a chromosome, but their order is unknown.
@@ -121,23 +170,6 @@ if __name__ == '__main__':
 	#Deletion?4 uncovers b and c. Which one of the following is the correct order for the genes?
 
 	random.shuffle(del_set)
+	print(makeHtmlTable(origlist, del_set))
 
-	sys.stderr.write("\n")
-	sys.stderr.write(("A total of %s genes "%(num2word[len(origlist)]))
-		+list2text(sortedlist)+" are closely linked in a single chromosome, but "
-		+"their order is unknown. ")
-	sys.stderr.write(("A total of %s deletions in the region are found "%(num2word[len(del_set)]))
-		+"to uncover recessive alleles of the genes as follows: ")
-	for i,deletion in enumerate(del_set):
-		sys.stderr.write("Deletion #%d uncovers the %s genes %s; "%(i+1, num2word[len(deletion)], list2text(deletion)))
-	sys.stderr.write("What the correct order for the %s genes? "%(num2word[len(origlist)]))
-	sys.stderr.write("Hint: the first gene on the end is gene %s.\n"%(origlist[0]))
-
-	sys.stderr.write("\n")
-	sys.stderr.write("Answer: %s\n"%(list2string(origlist)))
-	sys.stderr.write("\n")
-
-	#sys.stdout.write("Which one of the following is the correct order for the genes?")
-
-	sys.stderr.write("##########\n")
-
+	writeQuestion(origlist, del_set)
