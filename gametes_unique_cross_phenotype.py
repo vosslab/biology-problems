4f08846330fb91f717006b9f127def658de92d08
Neil Voss MacBookPro <vossman77@yahoo.com>
2021-10-04 22:37:25 -0500

    yes, more questions

--- gametes_unique_cross_phenotype.py
+++ gametes_unique_cross_phenotype.py
@@ -6,16 +6,18 @@ import genotype
 
 
 if __name__ == '__main__':
-	num_genes = 6
+	num_genes = 7
 	num_questions = 299
 	max_choices = 5
 	N = 0
-	f = open('bbq-gametes_unique_cross_genotype.txt', 'w')
+	choice_dict = {}
+	
+	f = open('bbq-gametes_unique_cross_phenotype.txt', 'w')
 	for i in range(num_questions):
 		N += 1
 		number = "{0}. ".format(N)
 		question = ""
-		question += "How many unique genotypes could be produced through hybrid cross between <ul> "
+		question += "How many unique <strong>phenotypes</strong> could be produced through hybrid cross between <ul> "
 		gamete_count1 = 1
 		while gamete_count1 < 2 or gamete_count1 > 16:
 			gene_list1 = genotype.createGenotypeList(num_genes)
@@ -24,7 +26,7 @@ if __name__ == '__main__':
 		while gamete_count2 < 2 or gamete_count2 > 16:
 			gene_list2= genotype.createGenotypeList(num_genes)
 			geno2, gamete_count2 = genotype.createGenotypeStringFromList(gene_list2)
-		total_genotypes = genotype.countGenotypesForCross(gene_list1, gene_list2)
+		total_genotypes = genotype.countPhenotypesForCross(gene_list1, gene_list2)
 
 
 		question1 = "<li>a male (&male;) individual with the genotype {0}</li>".format(geno1)
@@ -38,6 +40,8 @@ if __name__ == '__main__':
 
 		#print("total_genotypes=", total_genotypes)
 		power2, power3 = genotype.deconstructPowerOfNumber(total_genotypes)
+		if power2 + power3 < 2:
+			continue
 		choice_text = genotype.formatChoice(power2, power3)
 		answer = (total_genotypes, choice_text)
 		
@@ -46,15 +50,15 @@ if __name__ == '__main__':
 
 		letters = "ABCDEF"
 		choices = []
-		for diff_power2 in range(-2, 3):
-			for diff_power3 in range(-2, 3):
+		for diff_power2 in range(-3, 2):
+			for diff_power3 in range(-3, 2):
 				if diff_power2 == 0 and diff_power3 == 0:
 					continue
 				npow2 = power2 + diff_power2
 				npow3 = power3 + diff_power3
 				if npow2 < 0 or npow3 < 0:
 					continue
-				if npow2 + npow3 > num_genes:
+				if npow2 + npow3 > num_genes - 1:
 					continue
 				if npow2 + npow3 < 2:
 					continue
@@ -64,21 +68,27 @@ if __name__ == '__main__':
 
 		#limit choices
 		random.shuffle(choices)
-		choices = choices[:max_choices-1]
+		choices = choices[:max_choices]
+		choices.sort()
+		choices.pop(-1)
 		choices.append(answer)
 		choices.sort()
+
 		
 		for i, choice in enumerate(choices):
 			value, choice_text = choice
 			f.write(choice_text+'\t')
+			letter = letters[i]
 			if value == total_genotypes:
 				prefix = 'x'
 				f.write('Correct\t')
+				choice_dict[letter] = choice_dict.get(letter, 0) + 1
 			else:
 				prefix = ' '
 				f.write('Incorrect\t')
-			print('- [{0}] {1}. {2}'.format(prefix, letters[i], choice_text))
+			print('- [{0}] {1}. {2}'.format(prefix, letter, choice_text))
 		print("")
 		f.write('\n')
 	f.close()
+	print(choice_dict)
 
--- genetics-problems/genotype.py
+++ genetics-problems/genotype.py
@@ -7,6 +7,47 @@ import random
 uppercase = "ABCDEFGHJKMPQRSTVWXYZ"
 lowercase = "abcdefghjkmpqrstvwxyz"
 
+
+def deconstructPowerOfNumber(num):
+	temp_num = num
+	power2 = 0
+	power3 = 0
+	while temp_num % 2 == 0:
+		temp_num //= 2
+		power2 += 1
+	while temp_num % 3 == 0:
+		temp_num //= 3
+		power3 += 1
+	#print(num, power2, power3)
+	return power2, power3
+
+def formatChoice(power2, power3):
+	num = 2**power2 * 3**power3
+
+	pow2str = ""
+	if power2 == 0:
+		pow2str = None
+	if power2 == 1:
+		pow2str = "2"
+	if power2 > 1:
+		pow2str = "2<sup>{0}</sup>".format(power2)		
+	
+	pow3str = ""
+	if power3 == 0:
+		pow3str = None
+	if power3 == 1:
+		pow3str = "3"
+	if power3 > 1:
+		pow3str = "3<sup>{0}</sup>".format(power3)
+	
+	if pow2str is None:
+		mystr = pow3str + " &nbsp; = {0}".format(num)
+	elif pow3str is None:
+		mystr = pow2str + " &nbsp; = {0}".format(num)
+	else:
+		mystr = "{0} &times; {1} &nbsp; = {2}".format(pow2str, pow3str, num)
+	return mystr
+
 def crossGenotypes(geno1, geno2):
 	cross = []
 	for g1 in geno1:
@@ -17,9 +58,8 @@ def crossGenotypes(geno1, geno2):
 			cross.append(gstr)
 	cross.sort()
 	result = list(set(cross))
-	print(result)
+	#print(result)
 	return result
-
 
 def countGenotypesForCross(gene_list1, gene_list2):
 	if len(gene_list1) != len(gene_list2):
@@ -32,6 +72,35 @@ def countGenotypesForCross(gene_list1, gene_list2):
 		cross = crossGenotypes(geno1, geno2)
 		total_genotypes *= len(cross)
 	return total_genotypes
+
+
+def crossPhenotypes(geno1, geno2):
+	cross = []
+	#print(geno1, geno2)
+	for g1 in geno1:
+		for g2 in geno2:
+			g = [g1,g2]
+			g.sort()
+			gstr = g[0] #+g[1]
+			#print(g[0], g[1], gstr)
+			cross.append(gstr)
+	cross.sort()
+	result = list(set(cross))
+	#print(result)
+	return result
+
+def countPhenotypesForCross(gene_list1, gene_list2):
+	if len(gene_list1) != len(gene_list2):
+		print("different sizes of lists")
+		sys.exit(1)
+	total_genotypes = 1
+	for i in range(len(gene_list1)):
+		geno1 = gene_list1[i]
+		geno2 = gene_list2[i]
+		cross = crossPhenotypes(geno1, geno2)
+		total_genotypes *= len(cross)
+	return total_genotypes
+
 
 def createGenotypeList(num_genes):
 	gene_list = []
