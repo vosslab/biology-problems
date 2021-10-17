06da2314607c27e72ff7e195ffe842f1d06557ae
Neil Voss MacBookPro <vossman77@yahoo.com>
2021-10-17 09:45:29 -0500

    new stuff

---
+++ dna_gel_migration.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import csv
+import math
+import numpy
+import random
+
+class GelMigration(object):
+	def __init__(self):
+		self.debug = True
+		self.multiple_choice = True
+		self.slope = 0.8
+		self.intercept = 5.5
+		self.min_base_pairs = 100
+		self.max_base_pairs = 20000
+		self.createDNA_Tree()
+
+
+	#==================================================
+	def isValidDNA_Size(self, num_base_pairs):
+		num_zeros = math.floor(math.log10(num_base_pairs + 1))
+		two_divisor = 10**(num_zeros - 1)
+
+		first_two_digits = num_base_pairs // two_divisor
+		second_digit = first_two_digits % 10
+		if second_digit != 0 and second_digit != 5:
+			return False
+
+		other_digits = num_base_pairs % two_divisor
+		if other_digits > 0:
+			return False	
+
+		#print(num_base_pairs, 10**num_zeros, first_two_digits, other_digits)
+		return True
+
+	#==================================================
+	def getNearestValidDNA_Size(self, num_base_pairs):
+		num_zeros = math.floor(math.log10(num_base_pairs + 1))
+		two_divisor = 10**(num_zeros - 1)
+
+		first_two_digits = int(math.ceil(num_base_pairs / two_divisor))
+		first_digit = first_two_digits // 10
+		second_digit = first_two_digits % 10
+		if 2 < second_digit < 8:
+			second_digit = 5
+		else:
+			second_digit = 0
+		first_two_digits = first_digit * 10 + second_digit
+		new_num_base_pairs = first_two_digits * two_divisor
+		print(num_base_pairs, "-->", new_num_base_pairs)
+
+		return new_num_base_pairs
+
+	#==================================================
+	def createDNA_Tree(self):
+		self.dna_list = []
+		min_value = self.min_base_pairs
+		max_value = self.max_base_pairs + 1
+		step_value = self.min_base_pairs
+		for num_base_pairs in range(min_value, max_value, step_value):
+			if self.isValidDNA_Size(num_base_pairs):
+				self.dna_list.append(num_base_pairs)
+		if self.debug is True:
+			print("Created DNA {0:d} bands".format(len(self.dna_list)))
+		return
+
+	#==================================================
+	def molecular_weight_to_distance(self, mw):
+		dist = self.intercept - math.log(mw)*self.slope
+		return dist
+
+	#==================================================
+	def distance_to_molecular_weight(self, dist):
+		mw = math.exp( (self.intercept - dist)/self.slope )
+		return mw
+
+	#==================================================
+	def random_subset(self, size):
+		return random.sample(self.dna_list, size)
+
+	#==================================================
+	def get_dna_set_for_gel(self):
+		subset = self.random_subset(5)
+		subset.sort()
+		print(subset)
+		return subset
+
+	#==================================================
+	def get_unknown(self, subset):
+		array = numpy.array(subset, dtype=numpy.uint16)
+		#print(array)
+		logarray = numpy.log10(array)
+		#print(logarray)
+		diff = numpy.diff(logarray)
+		#print(diff)
+		arg = numpy.argmax(diff)
+		#print(arg)
+		#print(diff[arg])
+		#print(array[arg], array[arg+1])
+		#print(logarray[arg], logarray[arg+1])
+		new_log_band = logarray[arg] + diff[arg]/2.0
+		#print("new_log_band=", new_log_band)
+		new_band = int(math.ceil(10**new_log_band))
+		#print("new_band=", new_band)
+		new_valid_band = self.getNearestValidDNA_Size(new_band)
+		print("new_valid_band=", new_valid_band)
+		mw_range = 10**math.floor(new_log_band) // 2
+		unknown_dist = 0
+
+		if self.debug is True:
+			print("Unknown: MW = {0:.1f} +/- {1:.1f}; Dist = {2:.2f}".format(new_valid_band, mw_range, unknown_dist))
+		return new_valid_band, unknown_dist, mw_range
+
+
+	#==================================================
+	def writeProblem(self, N=44):
+		"""
+		49. <p>The standard and unknown proteins listed in the table were run using SDS–PAGE.</p> <p><b>Estimate the molecular weight of the unknown protein.</b></p>
+		A. 190 kDa	B. 320 kDa	C. 430 kDa	D. 520 kDa
+		"""
+
+		gel_set = self.get_dna_set_for_gel()
+		unknown_mw, unknown_dist, mw_range = self.get_unknown(gel_set)
+
+		if self.multiple_choice is True:
+			question = "{0:d}. ".format(N)
+		else:
+			question = ""
+		question += " <h6>Gel Migration Problem</h6> "
+		question += ('<p><table cellpadding="2" cellspacing="2" style="text-align:center; border: 1px solid black; font-size: 14px;">')
+		question += ('<tr><th>Protein Name</th><th>Molecular<br/>Weight (kDa)</th><th>Migration<br/>Distance (cm)</th></tr>')
+		for protein_dict in gel_set:
+			dist = self.molecular_weight_to_distance(protein_dict['MW'])
+			question += ('<tr><td>{0} ({1})</td><td align="center">{2:.1f}</td><td align="center">{3:.2f}</td></tr>'.format(protein_dict['fullname'], protein_dict['abbr'], protein_dict['MW'], dist))
+		question += ('<tr><td>{0}</td><td align="center">{1}</td><td align="center">{2:.2f}</td></tr>'.format("Unknown", "?", unknown_dist))
+		question += "</table></p>"
+		question += '<p>The standard and unknown proteins listed in the table were run using SDS&ndash;PAGE.</p>'
+		question += '<p><b>Estimate the molecular weight of the unknown protein.</b></p>'
+
+		if self.multiple_choice is True:
+			print(question)
+			choices = []
+			for i in range(4):
+				j = i+1
+				if j == gap:
+					choices.append(unknown_mw)
+					continue
+				mw, d, r, g = self.get_unknown(gel_set, j)
+				choices.append(mw)
+			letters = "ABCDEFG"
+			for i in range(4):
+				prefix = ''
+				if abs(choices[i] - unknown_mw) < 0.1:
+					prefix = '*'
+				print("{0}{1}. {2:.0f} kDa".format(prefix, letters[i], choices[i]))
+			print("")
+		else:
+			bb_format = self.format_for_blackboard(question, unknown_mw, mw_range)
+			print(bb_format)
+
+		return question
+
+	#==================================================
+	def format_for_blackboard(self, question, answer, tolerance):
+		#https://experts.missouristate.edu/plugins/servlet/mobile?contentId=63486780#content/view/63486780
+		#"NUM TAB question text TAB answer TAB [optional]tolerance"
+		return ("NUM\t{0}\t{1:.1f}\t{2:.1f}".format(question,answer,tolerance))
+
+
+
+#==================================================
+#==================================================
+if __name__ == '__main__':
+	total_problems = 100
+	gelm = GelMigration()
+	for question_count in range(total_problems):
+		problem = gelm.writeProblem(question_count+1)
+
+
+
--- fret_overlap_colors.py
+++ fret_overlap_colors.py
@@ -9,6 +9,7 @@ colors = ('violet (410 nm)', 'indigo (430 nm)', 'blue (460 nm)', 'green (530 nm)
 html_colors = ('#9d159d', '#4a0080', '#000080', '#006600', '#b3b300', '#e63900', '#990000')
 options = ('donor_absorb', 'fret_color', 'acceptor_emit')
 
+# THIS IS TYPE 2...
 #TYPE 1, the correct order of the colors
 #TYPE 2, the correct overlap of donor/acceptor C1 -> C2 and C3 -> C4; C2=C3, not C1=C4 or C1=C3 or C2=C4
 
@@ -18,17 +19,17 @@ def colorHtml(color_id):
 	return html_text
 
 #==================================
-def writeChoice(donor_absorb_id, fret_color_id, acceptor_emit_id):
+def writeChoice(color_seq):
 	choice = ""
-	choice += "The donor protein absorbs {0} and emits {1}; ".format(colorHtml(donor_absorb_id), colorHtml(fret_color_id))
-	choice += "the acceptor protein absorbs {0} and emits {1}. ".format(colorHtml(fret_color_id), colorHtml(acceptor_emit_id))
+	choice += "The donor protein absorbs {0} and emits {1}; ".format(colorHtml(color_seq[0]), colorHtml(color_seq[1]))
+	choice += "the acceptor protein absorbs {0} and emits {1}. ".format(colorHtml(color_seq[2]), colorHtml(color_seq[3]))
 	return choice
 
 #==================================
 #==================================
 if __name__ == '__main__':
 	answer_hist = {}
-	num_duplicates = 2
+	num_duplicates = 5
 	#==================================
 	indices = list(range(7))
 	set_indices = list(itertools.combinations(indices, 3))
@@ -49,8 +50,9 @@ if __name__ == '__main__':
 		
 	for color_set in filtered_set_indices:
 		if color_set[1] - color_set[0] == 1 or color_set[2] - color_set[1] == 1:
-			#make sure the colors are not next to one another
-			continue
+			#double check to make sure the colors are not next to one another
+			print("colors were not filtered")
+			sys.exit(1)
 		donor_absorb = colors[color_set[0]]
 		fret_color = colors[color_set[1]]
 		acceptor_emit = colors[color_set[2]]
@@ -63,18 +65,37 @@ if __name__ == '__main__':
 
 		letters = "ABCDEF"
 
-		permuations = list(itertools.permutations(color_set))
-		random.shuffle(permuations)
-		#print(permuations)
+		#TYPE 2, the correct overlap of donor/acceptor C1 -> C2 and C3 -> C4; C2=C3, not C1=C4 or C1=C3 or C2=C4
+		correct_seq = (color_set[0], color_set[1], color_set[1], color_set[2])
+
+		wrong1a_seq = (color_set[0], color_set[1], color_set[2], color_set[0])
+		wrong2a_seq = (color_set[0], color_set[1], color_set[0], color_set[2])
+		wrong3a_seq = (color_set[0], color_set[1], color_set[2], color_set[1])
+
+		wrong0b_seq = (color_set[2], color_set[1], color_set[1], color_set[0])
+		wrong1b_seq = (color_set[2], color_set[1], color_set[0], color_set[2])
+		wrong2b_seq = (color_set[2], color_set[1], color_set[2], color_set[0])
+		wrong3b_seq = (color_set[2], color_set[1], color_set[0], color_set[1])
+
+		color_sequences = [wrong1a_seq, wrong2a_seq, wrong3a_seq, wrong0b_seq, wrong1b_seq, wrong2b_seq, wrong3b_seq]
+		random.shuffle(color_sequences)
+		color_sequences.pop()
+		random.shuffle(color_sequences)
+		color_sequences.pop()
+		random.shuffle(color_sequences)
+		color_sequences.pop()
+		random.shuffle(color_sequences)
+		color_sequences.append(correct_seq)
+		random.shuffle(color_sequences)
 		
 		for d in range(num_duplicates):
-			random.shuffle(permuations)
+			random.shuffle(color_sequences)
 			f.write("MC\t{0}\t".format(question))
 			print("{0}. {1}".format(N, question))
-			for i, color_permute in enumerate(permuations):
-				choice = writeChoice(color_permute[0], color_permute[1], color_permute[2])
+			for i, color_seq in enumerate(color_sequences):
+				choice = writeChoice(color_seq)
 				f.write(choice+'\t')
-				if color_permute == color_set:
+				if color_seq == correct_seq:
 					prefix = 'x'
 					f.write('Correct\t')
 					answer_hist[letters[i]] = answer_hist.get(letters[i], 0) + 1
