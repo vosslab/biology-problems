dee8656726a12b91eb05c635ba122d266d0a67b2
Neil Voss <vossman77@yahoo.com>
2021-10-31 11:27:03 -0500

    some edits

--- inheritance-problems/pedigree_lib.py
+++ inheritance-problems/pedigree_lib.py
@@ -7,28 +7,27 @@ space = " "
 dash = '_'
 bar = '|'
 
-#https://arxiv.org/pdf/1902.03321.pdf
-
+#===============================
 def makeCharacterTD_Cell(character_name):
 	character_unicode = character_unicodes[character_name]
 	fontsize = character_sizes[character_name]
 	html_text = makeTD_Cell(character_name, character_unicode, fontsize)
 	return html_text
 
+#===============================
 def makeTD_Cell(comment_text, character_unicode, fontsize):
-	character_unicode = character_unicodes[character_name]
-	fontsize = character_sizes[character_name]
 	html_text = ''
-	if comment_name is not None:
+	if comment_text is not None:
 		html_text += '<!--- {0} ---> '.format(comment_text)
 	html_text += '<td align="center" style="'
 	if fontsize is not None:
-		'font-size: {0:d}pt; '.format(fontsize)
+		html_text += 'font-size: {0:d}pt; '.format(fontsize)
 	html_text += 'vertical-align: middle; width: 80px; height: 80px;">'
 	html_text += '{0}</td>'.format(character_unicode)
 	return html_text
 
 
+#===============================
 character_sizes = {
 	'LEFT-HALF BLACK SQUARE': 42,
 	'RIGHT-HALF BLACK SQUARE': 42,
@@ -40,19 +39,22 @@ character_sizes = {
 	'WHITE CIRCLE': 28,
 }
 
+#===============================
 character_unicodes = {
 	'LEFT-HALF BLACK SQUARE': '&#x25E7;',
 	'RIGHT-HALF BLACK SQUARE': '&#x25E8;',
-	'BLACK SQUARE': '&#x25FB;',
-	'WHITE SQUARE': '&#x25FC;',
+	'BLACK SQUARE': '&#x25FC;',
+	'WHITE SQUARE': '&#x25FB;',
 	'LEFT-HALF BLACK CIRCLE': '&#x25D0;',
 	'RIGHT-HALF BLACK CIRCLE': '&#x25D1;',
 	'BLACK CIRCLE': '&#x2B24;',
 	'WHITE CIRCLE': '&xcirc;',
 }
 
+#===============================
 short_hand_lookup = {
 	'.': 'SPACE',
+	'%': 'NEW LINE',
 	'x': 'BLACK SQUARE',
 	'#': 'WHITE SQUARE',
 	'*': 'BLACK CIRCLE',
@@ -68,15 +70,28 @@ short_hand_lookup = {
 	'd': 'DOWN-LEFT ELBOW SHAPE',
 }
 
-
+#===============================
 def makeShapeNameTableTD_Cell(shape_name):
-	pass
+	tent = """
+<! --- START TENT SHAPE --->
+<td align="center" style="vertical-align: middle;">
+<table border="0" cellpadding="0" cellspacing="0"
+  style="width: 80px; height: 80px; border-collapse: collapse; border-style: hidden;">
+<tbody><tr>
+<td style="border-bottom: 3px solid #000000; border-right: 3px solid #000000;">&nbsp;</td>
+<td style="border-bottom: 3px solid #000000; border-left: 3px solid #000000;">&nbsp;</td>
+</tr><tr>
+<td style="border-top: 3px solid #000000; ">&nbsp;</td>
+<td style="border-top: 3px solid #000000; ">&nbsp;</td>
+</tr></tbody></table></td>
+<! --- END TENT SHAPE --->
+"""
+	return tent
 
 #===============================
-
 def translateCode(code_string):
 	html_code = '<table><tr>'
-	for char in List(code_string):
+	for char in list(code_string):
 		char_name = short_hand_lookup[char]
 		if char_name.endswith('SHAPE'):
 			html_code += makeShapeNameTableTD_Cell(char)
@@ -84,19 +99,29 @@ def translateCode(code_string):
 			html_code += makeCharacterTD_Cell(char_name)
 		elif char_name == 'SPACE':
 			html_code += makeTD_Cell(None, '&nbsp;', None)
+		elif char_name == 'NEW LINE':
+			html_code += '</tr><tr>'
+	html_code += '</tr></table>'
 	return html_code
 
+
+#===============================
+#===============================
+#===============================
+#===============================
 if __name__ == '__main__':
-	code_string = (	 "#To..#To"
-					+"d^r...|."
-					+"x.*-T-#."
-					+"....|..."
+	code_string = (	 "#To..#To%"
+					+"d^r...|.%"
+					+"x.*-T-#.%"
+					+"....|...%"
 					+"....x..."
 				)
 	code = '<table><tr>'
 	for name in character_sizes.keys():
 		code += makeCharacterTD_Cell(name)
 	code += '</tr></table>'
+	#print(code)
+	code = translateCode(code_string)
 	print(code)
 
 #THE END
