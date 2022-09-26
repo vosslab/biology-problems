136cd975068ba65c8a0550e0365f4d77f759dcd3
Neil Voss <vossman77@yahoo.com>
2022-09-26 12:58:53 -0500

    modernize, fix wording of question

--- molecular_biology-problems/linear_digest.py
+++ molecular_biology-problems/linear_digest.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import time
 import random
+import bptools
 import restrictlib
+
+debug = True
 
 #============================================
 #============================================
@@ -139,63 +143,135 @@ def getRandList(size, total_length, include_ends=False):
 
 #============================================
 #============================================
-#============================================
-if __name__ == '__main__':
-	length = 10
-	fragment = True
+def writeQuestion(N=1, length=10, num_sites=2, fragment=True):
+
 	enzymes = restrictlib.get_enzyme_list()
 	enzyme_class1 = restrictlib.random_enzyme_one_end(enzymes)
 	enzyme_name1 = enzyme_class1.__name__
 	enzyme_class2 = restrictlib.random_enzyme_one_end(enzymes, badletter=enzyme_name1[0])
 	enzyme_name2 = enzyme_class2.__name__
 
+	if fragment is False and num_sites < 3:
+		print("Strand mode requires at least 3 sites")
+		sys.exit(1)
+	elif fragment is True and num_sites < 2:
+		print("Fragment mode requires at least 2 sites")
+		sys.exit(1)
+
 	answers = []
+	#Fragment
+	complete_sites = num_sites + (num_sites - 1)
+	if complete_sites * 2 > length:
+		print(complete_sites, "too many sites for length", length)
+		return None
+	sites = getRandList(complete_sites, length, include_ends=False)
+	label_dict = {}
+	for i, site in enumerate(sites):
+		if i % 2 == 0:
+			label_dict[site] = enzyme_name1
+			if debug is True:
+				print(site, enzyme_name1)
+		else:
+			label_dict[site] = enzyme_name2
+			if debug is True:
+				print(site, enzyme_name2)
+	if debug is True:
+		print("end", length)
+
+
+	# GENERATE ANSWERS
+	strand_answers = []
+	fragment_answers = [sites[0], length-sites[-1]]
+	for i in range(0, len(sites)-2, 2):
+		answer_length = sites[i+2] - sites[i]
+		fragment_answers.append(answer_length)
+		strand_answers.append(answer_length)
+	if debug is True:
+		strand_answers.sort()
+		fragment_answers.sort()
+		print("RAW fragment_answers=", fragment_answers)
+		print("RAW strand_answers=", strand_answers)
+
+	strand_answers = list(set(strand_answers))
+	strand_answers.sort()
+	fragment_answers = list(set(fragment_answers))
+	fragment_answers.sort()
+	if fragment_answers == strand_answers:
+		print("Fragment answers match strand answers")
+		print(fragment_answers, strand_answers)
+		time.sleep(0.1)
+		return None
+
 	if fragment is True:
-		a = getRandList(3, length, include_ends=False)
-		label_dict = {
-			a[0]: enzyme_name1,
-			a[1]: enzyme_name2,
-			a[2]: enzyme_name1,
-		}
-		answers = [a[0], a[2]-a[0], length-a[2]]
-	else:
-		a = getRandList(5, length, include_ends=False)
-		label_dict = {
-			a[0]: enzyme_name1,
-			a[1]: enzyme_name2,
-			a[2]: enzyme_name1,
-			a[3]: enzyme_name2,
-			a[4]: enzyme_name1,
-		}
-		answers = [a[2]-a[0], a[4]-a[2]]
-	answers.sort()
-	print(answers)
+		answers = fragment_answers
+	elif fragment is False:
+		answers = strand_answers
+	if len(answers) < 2:
+		print("too few fragments")
+		print(answers)
+		time.sleep(0.1)
+		return None
+
+	if debug is True:
+		strand_answers.sort()
+		fragment_answers.sort()
+		print("TRIM fragment_answers=", fragment_answers)
+		print("TRIM strand_answers=", strand_answers)
 
 	table = makeTable(length, label_dict, fragment)
-	header = "<p>Below is a linear piece of DNA that is {0:d} kb in length.</p> ".format(length)
-	details = "<p>There are two (2) different types of restriction sites: {0} and {1} labeled at the top of the DNA strand.</p>".format(enzyme_name1, enzyme_name2)
+	if fragment is True:
+		header = "<p>Below is a short fragment of DNA that is {0:d} kb in length.</p> ".format(length)
+	elif fragment is False:
+		header = "<p>Below is a long strand of DNA, a close up of particular section is shown.</p> "
+	details  = "<p>There are two (2) different types of restriction sites: "
+	details += "<i>{0}</i> and <i>{1}</i> labeled at the top of the DNA strand.</p>".format(enzyme_name1, enzyme_name2)
 	if fragment is False:
-		details += "<p>The dashes at the left and right indicate that the next restriction site is very far away and will not be present on a gel.</p>"
-	question = "<h6>Determine the sizes of bands that would appear on a gel after restriction enzyme digestion with only <i>{0}</i>.</h6>".format(enzyme_name1)
-	print(header+details+question)
+		details += "<p>Note: The dashes at the left and right indicate that the next restriction site is"
+		details += "very far away and will not be appear in a gel; it will likely be stuck in the well.</p>"
+	question = "<h6>Determine the sizes of bands that would appear on a gel "
+	question += "after restriction enzyme digestion with only <i>{0}</i>.</h6>".format(enzyme_name1)
+	#print(header+details+question)
 	#print(header+table+details+question)
 
-	max_answer = max(answers)
+	max_answer = max(fragment_answers)
 	answer_str = ""
 	last_answer = max(max_answer+1, 5)
+	choices_list = []
+	answers_list = []
 	for i in range(1, last_answer+1):
-		answer_str += "{0} kb\t".format(i)
+		choice_str = "{0:d} kb".format(i)
+		choices_list.append(choice_str)
 		if i in answers:
-			answer_str += "Correct\t"
-		else:
-			answer_str += "Incorrect\t"
-	print(answer_str)
+			answers_list.append(choice_str)
 	#sys.exit(1)
+	question_text = header+table+details+question
+	bb_question = bptools.formatBB_MA_Question(N, question_text, choices_list, answers_list)
+	return bb_question
+
+#============================================
+#============================================
+#============================================
+if __name__ == '__main__':
+	num_questions = 10
+	length=18
+	num_sites=4
+	fragment=False
+	print("num_questions=", num_questions)
+	print("length=", length)
+	print("num_sites=", num_sites)
+	print("fragment=", fragment)
+
+
+
 	outfile = 'bbq-' + os.path.splitext(os.path.basename(__file__))[0] + '-questions.txt'
 	print('writing to file: '+outfile)
-	f = open(outfile, 'a')
-	f.write("MA\t")
-	f.write(header+table+details+question+"\t")
-	f.write(answer_str)
-	f.write('\n')
+	f = open(outfile, 'w')
+	N = 0
+	for i in range(num_questions):
+		bb_question = writeQuestion(N, length, num_sites, fragment)
+		if bb_question is not None:
+			N += 1
+			f.write(bb_question)
 	f.close()
+	bptools.print_histogram()
+	print("Wrote", N, "of", num_questions, "questions")
