90b31079b3197a9e16a78705e79f910e6a310986
Neil Voss <vossman77@yahoo.com>
2022-10-30 21:49:56 -0500

    instead of using key combintations, use list of values to generate questions

--- matching_sets/yaml_make_which_one_multiple_choice.py
+++ matching_sets/yaml_make_which_one_multiple_choice.py
@@ -69,7 +69,7 @@ def makeQuestions(yaml_data, num_choices=None):
 					excluded_comb = True
 			if excluded_comb is False:
 				filter_combs.append(comb)
-		print('Filtered {0} combinations from {1} items'.format(len(filter_combs), len(all_combs)))
+		print('Filtered down to {0} combinations from {1} items'.format(len(filter_combs), len(all_combs)))
 		all_combs = filter_combs
 	random.shuffle(all_combs)
 	N = 0
@@ -81,6 +81,64 @@ def makeQuestions(yaml_data, num_choices=None):
 		if isinstance(answer_description, list):
 			answer_description = random.choice(answer_description)
 		random.shuffle(choices_list)
+		question = ("<p>Which one of the following {0} correspond to the description <strong>'{1}'</strong>.</p>".format(
+			yaml_data['key description'], answer_description))
+		N += 1
+		complete_question = bptools.formatBB_MC_Question(N, question, choices_list, answer)
+
+		list_of_complete_questions.append(complete_question)
+
+	list_of_complete_questions = applyReplacementRulesToQuestions(list_of_complete_questions, yaml_data.get('replacement_rules'))
+	return list_of_complete_questions
+
+#=======================
+def makeQuestions2(yaml_data, num_choices=None):
+	matching_pairs_dict = yaml_data['matching pairs']
+	exclude_pairs_list = yaml_data.get('exclude pairs', [])
+
+	list_of_complete_questions = []
+
+	if num_choices is None:
+		num_choices = yaml_data.get('items to match per question', 5)
+	### Get Keys and Key/Values Pairs
+	all_keys = list(matching_pairs_dict.keys())
+	key_value_pairs = []
+	for key in all_keys:
+		for value in matching_pairs_dict[key]:
+			pair = (key, value)
+			key_value_pairs.append(pair)
+	### Generate Combiniations
+	all_combs = list(itertools.combinations(all_keys, num_choices))
+	print('Created {0} combinations from {1} items'.format(len(all_combs), len(all_keys)))
+	#filter combinations
+	if len(exclude_pairs_list) > 0:
+		filter_combs = []
+		for comb in all_combs:
+			excluded_comb = False
+			#print(comb)
+			for a,b in exclude_pairs_list:
+				if a in comb and b in comb:
+					excluded_comb = True
+			if excluded_comb is False:
+				filter_combs.append(comb)
+		print('Filtered down to {0} combinations from {1} items'.format(len(filter_combs), len(all_combs)))
+		all_combs = filter_combs
+	random.shuffle(all_combs)
+	N = 0
+	for pair in key_value_pairs:
+		key, value = pair
+		comb = random.choice(all_combs)
+		count = 0
+		while not key in comb:
+			comb = random.choice(all_combs)
+			count += 1
+			if count > 20:
+				print("something probably wrong, too many combinations searched")
+				sys.exit(1)
+		choices_list = list(comb)
+		choices_list.sort()
+		answer = key
+		answer_description = value
 		question = ("<p>Which one of the following {0} correspond to the description <strong>'{1}'</strong>.</p>".format(
 			yaml_data['key description'], answer_description))
 		N += 1
@@ -114,7 +172,7 @@ if __name__ == '__main__':
 
 	list_of_complete_questions = []
 	for i in range(args.duplicate_runs):
-		list_of_complete_questions += makeQuestions(yaml_data, args.num_choices)
+		list_of_complete_questions += makeQuestions2(yaml_data, args.num_choices)
 
 	if len(list_of_complete_questions) > args.max_questions:
 		print("Too many questions, trimming down to {0} questions".format(args.max_questions))
