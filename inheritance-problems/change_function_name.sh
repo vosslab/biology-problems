15c9c9eeb7d3f9076815187d4ab210b1b98ed1fb
Neil Voss <vossman77@yahoo.com>
2023-10-31 06:32:10 -0500

    new

--- inheritance-problems/change_function_name.sh
+++ inheritance-problems/change_function_name.sh
@@ -8,13 +8,12 @@ for file in t*_test-cross_*.py unordered*.py; do
 
     # Use sed to find and replace the function call and store in temporary file
     cp "$file" "$temp_file"
-    sed 's/ invertType(/ ptcl.invert_genotype(/' "$temp_file" > "$temp_file"
-    sed 's/= ptcl.invert_type(/= ptcl.invert_genotype(/' "$temp_file" > "$temp_file"
-    sed 's/ flipGene(/ ptcl.flip_gene_by_letter(/' "$temp_file" > "$temp_file"
-    sed 's/= ptcl.flip_gene(/= ptcl.flip_gene_by_letter(/' "$temp_file" > "$temp_file"
-    sed 's/ getPhenotype(/ ptcl.get_phenotype_name(/' "$temp_file" > "$temp_file"
-    sed 's/= ptcl.get_phenotype(/= ptcl.get_phenotype_name(/' "$temp_file" > "$temp_file"
-    sed 's/ getGeneOrder(/ ptcl.get_random_gene_order(/' "$temp_file" > "$temp_file"
+    # Use sed to find and replace the function calls and update the temp file
+    gsed -e 's/invertType(/ptcl.invert_genotype(/' \
+        -e 's/flipGene(/ptcl.flip_gene_by_letter(/' \
+        -e 's/getPhenotype(/ptcl.get_phenotype_name(/' \
+        -e 's/getGeneOrder(/ptcl.get_random_gene_order(/' \
+        -i "$temp_file"
 
     # Get the number of lines in the original and temporary files
     original_lines=$(wc -l < "$file")
