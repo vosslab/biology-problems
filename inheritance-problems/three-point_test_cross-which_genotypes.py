936188d644a7a21f9fb4ae4c16e177dc7bab36cb
Neil Voss <vossman77@yahoo.com>
2023-11-05 22:13:53 -0600

    add find recombinants for genes X and Y

--- inheritance-problems/three-point_test_cross-which_genotypes.py
+++ inheritance-problems/three-point_test_cross-which_genotypes.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import random
 import argparse
 
 import bptools
@@ -11,8 +12,7 @@ debug = False
 
 #=====================
 #=====================
-def get_question_text(question_type='parental', gene=None):
-
+def get_question_text(question_type='parental', gene_pair=None):
 	question_string = ''
 	question_string += '<p>The resulting phenotypes are summarized in the table above.</p> '
 	question_string += '<h6>Question</h6> '
@@ -25,17 +25,17 @@ def get_question_text(question_type='parental', gene=None):
 		question_string += '<span style="color: DarkGreen;"><strong>'
 		question_string +=   f'{question_type} crossover genotype combinations</strong></span>. '
 		question_string +=   'These allele combinations are a result of two genetic crossover events.</p> '
-	elif question_type == 'gene' and gene is not None:
-		question_string += f'determine the recombinant types for gene {gene.upper()}.</p> '
+	elif question_type == 'genes' and gene_pair is not None:
+		gene_pair_text =   f'genes <b>{gene_pair[0].upper()}</b> and <b>{gene_pair[1].upper()}</b>'
+		question_string += '<span style="color: DarkBlue;"><strong>'
+		question_string +=   f'all recombinant genotypes</strong></span> for {gene_pair_text}. '
+		question_string += f'These genotypes result from crossover events that occur between the two {gene_pair_text} during meiosis.</p> '
 	else:
 		print('failed question type')
 		raise ValueError
-	question_string += '<p>More than one combination will be correct. '
+	question_string += '<p>More than one genotype will be correct. '
 	question_string += 'Select all that apply.</p> '
-
-
 	return question_string
-
 
 #=====================
 #=====================
@@ -44,13 +44,13 @@ if __name__ == "__main__":
 	question_group = parser.add_mutually_exclusive_group()
 	# Add question type argument with choices
 	question_group.add_argument('-t', '--type', dest='question_type', type=str,
-		choices=('parental', 'double', 'gene'), help='Set the question type: accept or reject')
+		choices=('parental', 'double', 'genes'), help='Set the question type: accept or reject')
 	question_group.add_argument('-p', '--parental', dest='question_type', action='store_const',
 		const='parental',)
 	question_group.add_argument('-D', '--double', dest='question_type', action='store_const',
 		const='double',)
-	question_group.add_argument('-g', '--gene', dest='question_type', action='store_const',
-		const='gene',)
+	question_group.add_argument('-g', '--genes', dest='question_type', action='store_const',
+		const='genes',)
 	parser.add_argument('-d', '--duplicates', metavar='#', type=int, dest='duplicates',
 		help='number of duplicate runs to do', default=1)
 	args = parser.parse_args()
@@ -84,19 +84,22 @@ if __name__ == "__main__":
 		double_cross_genotypes_tuple = (sorted_genotype_counts[-1][0], sorted_genotype_counts[-2][0])
 
 		choices_list = sorted(GMC.genotype_counts.keys(), reverse=True)
-		gene = None
+		gene_pair = None
 		if args.question_type == 'parental':
 			answers_list = list(GMC.parental_genotypes_tuple)
 		elif args.question_type == 'double':
 			answers_list = list(double_cross_genotypes_tuple)
-		elif args.question_type == 'gene':
-			#answers_list = list(type_categories[gene])
-			raise NotImplementedError
+		elif args.question_type == 'genes':
+			genes_list = list(GMC.gene_letters_str)
+			random.shuffle(genes_list)
+			gene_pair = tuple(genes_list[0:2])
+			answers_list = GMC.get_all_recombinants_for_gene_letter(gene_pair)
 		else:
 			raise ValueError(f'unknown question type: {args.question_type}')
 
-		question_string = get_question_text(args.question_type, gene)
+		question_string = get_question_text(args.question_type, gene_pair)
 		full_question = header + phenotype_info_text + html_table + question_string
+		GMC.is_valid_html(full_question)
 		final_question = bptools.formatBB_MA_Question(N, full_question, choices_list, answers_list)
 
 		f.write(final_question)
