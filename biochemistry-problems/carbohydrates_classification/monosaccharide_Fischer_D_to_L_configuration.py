5da91fa7144646f4cb481e1c6f53d32bf3742b9b
Neil Voss <vossman77@yahoo.com>
2025-03-28 10:47:34 -0500

    modernize code

--- biochemistry-problems/carbohydrates_classification/monosaccharide_Fischer_D_to_L_configuration.py
+++ biochemistry-problems/carbohydrates_classification/monosaccharide_Fischer_D_to_L_configuration.py
@@ -3,9 +3,11 @@
 import os
 import sys
 import random
+import bptools
 import sugarlib
 
-def makeQuestion(sugar_name):
+def write_question(N, sugar_name):
+	sugar_codes_class = sugarlib.SugarCodes()
 	sugar_code = sugar_codes_class.sugar_name_to_code[sugar_name]
 	sugar_struct = sugarlib.SugarStructure(sugar_code)
 	print(sugar_struct.structural_part_txt())
@@ -43,22 +45,38 @@ def makeQuestion(sugar_name):
 	if prelen != postlen:
 		sys.exit(1)
 
-	content = 'MC\t<p>{0}&xrarr;{1}</p>'.format(sugar_name, L_sugar_name)
-	content += fischer
-	content += question
+	sugar_names_text = f"<p>{sugar_name}&xrarr;{L_sugar_name}</p>"
+	question_content = sugar_names_text + fischer + question
 
+	html_fischer_choices_list = []
 	random.shuffle(choice_codes)
 	for s in choice_codes:
 		my_sugar_struct = sugarlib.SugarStructure(s)
 		my_fischer = my_sugar_struct.Fischer_projection_html()
-		content += "\t"+my_fischer
+		html_fischer_choices_list.append(my_fischer)
 		if s == answer_code:
-			content += "\tCorrect"
-		else:
-			content += "\tIncorrect"
-	return content
+			html_fischer_answer_text = my_fischer
 
-if __name__ == '__main__':
+	# Format the question for Blackboard
+	complete_question = bptools.formatBB_MC_Question(
+		N, question_content, html_fischer_choices_list, html_fischer_answer_text
+	)
+	return complete_question
+
+#======================================
+#======================================
+def parse_arguments():
+	"""
+	Parses command-line arguments for the script.
+	"""
+	parser = argparse.ArgumentParser(description="Generate questions.")
+
+	args = parser.parse_args()
+	return args
+
+#======================================
+#======================================
+def get_sugar_codes():
 	sugar_codes_class = sugarlib.SugarCodes()
 	D_hexose_names = sugar_codes_class.get_D_hexoses()
 	#D_hexose_names.remove('D-ribose')
@@ -68,13 +86,40 @@ if __name__ == '__main__':
 	D_hexose_names.remove('D-idose')
 
 	#D_hexose_names = sugar_codes_class.get_D_aldohexoses()
-	outfile = 'bbq-' + os.path.splitext(os.path.basename(__file__))[0] + '-questions.txt'
-	print('writing to file: '+outfile)
-	f = open(outfile, 'w')
-	for sugar_name in D_hexose_names:
-		#random.shuffle(D_hexose_names)
-		#sugar_name = D_hexose_names.pop()
-		print(sugar_name)
-		content = makeQuestion(sugar_name)
-		f.write(content+'\n')
-	f.close()
+	print(f"Retrieved {len(D_hexose_names)} from the sugar library")
+	return D_hexose_names
+
+#======================================
+#======================================
+def main():
+
+	# Define output file name
+	script_name = os.path.splitext(os.path.basename(__file__))[0]
+	outfile = (
+		'bbq'
+		f'-{script_name}'
+		'-questions.txt'
+	)
+	print(f'Writing to file: {outfile}')
+
+	sugar_names_list = get_sugar_codes()
+
+	# Open the output file and generate questions
+	with open(outfile, 'w') as f:
+		N = 1  # Question number counter
+		for sugar_name in sugar_names_list:
+			complete_question = write_question(N, sugar_name)
+			if complete_question is not None:
+				N += 1
+				f.write(complete_question)
+
+	# Display histogram if question type is multiple choice
+	bptools.print_histogram()
+
+#======================================
+#======================================
+if __name__ == '__main__':
+	main()
+
+## THE END
+
